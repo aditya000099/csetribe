177b990bd607cd9766fb825159c3dc1300586063
aditya000099 <adi.agg099@gmail.com>
2023-06-29 22:15:44 +0530

    forchecks

--- script/home.js
+++ script/home.js
@@ -1,4 +1,10 @@
 function viewCASA() {
     window.location.href = "files-pages/casa.html";
 }
+function viewMCP() {
+    window.location.href = "files-pages/mcp.html";
+}
+function viewBEE() {
+    window.location.href = "files-pages/bee.html";
+}
 
